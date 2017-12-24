c7a7f3aba16baf2ff3769d8b81ba03703524925d
slayershunter <slayershunter@narod.ru>
2017-12-24 03:28:04 +0300

    stage 8 (enconding shit)

---
+++ 8_shortening_of_name(3of10).php
@@ -0,0 +1,15 @@
+<?php
+// Вашим заданием будет создание сокращенного варианта ФИО.
+// Например, вводим: Иванов Иван Петрович, а в результате должны получить: Иванов И. П.
+
+function mb_str_split($string) { 
+    return preg_split('#(?<!^)(?!$)#u', $string); 
+}  
+
+$fullname = readline("Введите ФИО целиком: ");
+$splitname = explode(" ", $fullname);
+$firstname = mb_str_split($splitname[1]);
+$patronymic = mb_str_split($splitname[2]);
+
+print "Сокращенное ФИО: $splitname[0] $firstname[0]. $patronymic[0].\n";
+?>
